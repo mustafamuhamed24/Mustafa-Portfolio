27727893baa3392030c93ed19533dd2a86312796
mustafamuhamed24 <119931967+mustafamuhamed24@users.noreply.github.com>
2025-02-10 21:42:09 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -136,66 +136,22 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Projects Will Uploaded Soon",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: carrent,
+      "Projects Under Development",
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "Projects Will Uploaded Soon",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
+      "Projects Under Development",
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Projects Will Uploaded Soon",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
+      "Projects Under Development",
     ],
-    image: tripguide,
     source_code_link: "https://github.com/",
   },
 ];
